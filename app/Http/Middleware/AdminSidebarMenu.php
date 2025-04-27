682c1fbc5d91235dbd67377461df525d73ed8c01
developedbyabhi <developwithabhi@gmail.com>
2025-04-27 09:13:22 +0530

    fix

--- app/Http/Middleware/AdminSidebarMenu.php
+++ app/Http/Middleware/AdminSidebarMenu.php
@@ -297,9 +297,7 @@ class AdminSidebarMenu
                 $menu->dropdown(
                     __('sale.sale'),
                     function ($sub) use ($enabled_modules, $is_admin, $pos_settings) {
-                        dd(auth()->user()->permissions);
                         if (!empty($pos_settings['enable_sales_order']) && ($is_admin || auth()->user()->hasAnyPermission(['so.view_own', 'so.view_all','so.view_verified_only', 'so.create']))) {
-                            dd("pass");
                             $sub->url(
                                 action([\App\Http\Controllers\SalesOrderController::class, 'index']),
                                 __('lang_v1.sales_order'),

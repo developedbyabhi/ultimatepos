bb6da606f28635f519522c4bb47c8f15eac6ea79
developedbyabhi <developwithabhi@gmail.com>
2025-04-27 08:49:15 +0530

    VIEW PERMISION FIX

--- app/Http/Middleware/AdminSidebarMenu.php
+++ app/Http/Middleware/AdminSidebarMenu.php
@@ -297,7 +297,7 @@ class AdminSidebarMenu
                 $menu->dropdown(
                     __('sale.sale'),
                     function ($sub) use ($enabled_modules, $is_admin, $pos_settings) {
-                        if (!empty($pos_settings['enable_sales_order']) && ($is_admin || auth()->user()->hasAnyPermission(['so.view_own', 'so.view_all', 'so.create']))) {
+                        if (!empty($pos_settings['enable_sales_order']) && ($is_admin || auth()->user()->hasAnyPermission(['so.view_own', 'so.view_all','so.view_verified_only', 'so.create']))) {
                             $sub->url(
                                 action([\App\Http\Controllers\SalesOrderController::class, 'index']),
                                 __('lang_v1.sales_order'),
